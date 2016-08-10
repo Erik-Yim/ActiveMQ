8bf4816cd86648ba3ca5888e207bd17aa4da22cb
zhangtian <zhangtian@happy>
2016-08-10 19:25:12 +0800

    发布订阅  多个消费者订阅主题

---
+++ activemq-simple/src/main/java/cn/itcast/adcivemq/demo02/JMSConsumer.java
@@ -0,0 +1,54 @@
+package cn.itcast.adcivemq.demo02;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+// 发布订阅 消息消费者  需要消费者提前订阅   否则复发获取发布的消息  可以有多个
+public class JMSConsumer {
+	private static final String USERNAME = ActiveMQConnection.DEFAULT_USER;// 默认的连接的用户名
+	private static final String PASSWORD = ActiveMQConnection.DEFAULT_PASSWORD;// 默认的连接密码
+	private static final String BROKEURL = ActiveMQConnection.DEFAULT_BROKER_URL;// 默认的连接地址
+	private static final int SENDNUM = 10;// 发送消息的数量
+	
+	public static void main(String[] args) {
+		ConnectionFactory connectionFactory;// 连接工厂
+		Connection connection = null;// 连接
+		Session session;// 会话 发送或者接受消息的线程
+		Destination destination;// 消息的目的地
+		MessageConsumer consumer;
+
+		connectionFactory = new ActiveMQConnectionFactory(JMSConsumer.USERNAME, JMSConsumer.PASSWORD,
+				JMSConsumer.BROKEURL);
+		try {
+			// 通过连接工厂获取连接
+			connection = connectionFactory.createConnection();
+			// 启动连接
+			connection.start();
+			session = connection.createSession(false,Session.AUTO_ACKNOWLEDGE );//创建消息队列
+			destination = session.createTopic("FirstTopic");//创建主题
+			consumer = session.createConsumer(destination);
+			/*通过receive方法接收消息：一般不用receive ，而是用监听
+			while(true){
+				TextMessage message =  (TextMessage) consumer.receive(100000);//100秒获取一次
+				if(message.getText()!=null){
+					System.out.println("收到的消息："+message.getText());
+				}else{
+					break;
+				}
+			}*/
+			//为消息消费者注册监听器
+			consumer.setMessageListener(new MessageListener());
+			
+			
+		} catch (JMSException e) {
+			e.printStackTrace();
+		} 
+	}
+}
---
+++ activemq-simple/src/main/java/cn/itcast/adcivemq/demo02/MessageListener.java
@@ -0,0 +1,63 @@
+package cn.itcast.adcivemq.demo02;
+
+import java.util.Enumeration;
+
+import javax.jms.BytesMessage;
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.ObjectMessage;
+import javax.jms.StreamMessage;
+import javax.jms.TextMessage;
+// 消息监听器
+public class MessageListener implements javax.jms.MessageListener {
+	@Override
+	public void onMessage(Message msg) {
+		try {
+			if(msg.getClass().isAssignableFrom(TextMessage.class)) {
+				TextMessage message = (TextMessage) msg;
+                System.out.println("------Received TextMessage------");
+                System.out.println(message.getText());
+			} else if(msg.getClass().isAssignableFrom(MapMessage.class)) {
+				MapMessage message = (MapMessage) msg;
+                System.out.println("------Received MapMessage------");
+                System.out.println(message.getLong("long"));
+                System.out.println(message.getBoolean("boolean"));
+                System.out.println(message.getShort("short"));
+                System.out.println(message.getString("MapMessage"));
+                System.out.println("------Received MapMessage for while------");
+                Enumeration enumer = message.getMapNames();
+                while (enumer.hasMoreElements()) {
+                    Object obj = enumer.nextElement();
+                    System.out.println(message.getObject(obj.toString()));
+                }
+			} else if(msg.getClass().isAssignableFrom(StreamMessage.class)) {
+				StreamMessage message = (StreamMessage) msg;
+                System.out.println("------Received StreamMessage------");
+                System.out.println(message.readString());
+                System.out.println(message.readBoolean());
+                System.out.println(message.readLong());
+			} else if(msg.getClass().isAssignableFrom(ObjectMessage.class)) {
+				System.out.println("------Received ObjectMessage------");
+                ObjectMessage message = (ObjectMessage) msg;
+                MqMessage mqMessage = (MqMessage) message.getObject();
+                System.out.println("name: " + mqMessage.getName());
+			} else if (msg.getClass().isAssignableFrom(BytesMessage.class)) {
+				System.out.println("------Received BytesMessage------");
+                BytesMessage message = (BytesMessage) msg;
+                byte[] byteContent = new byte[1024];
+                int length = -1;
+                StringBuffer content = new StringBuffer();
+                while ((length = message.readBytes(byteContent)) != -1) {
+                    content.append(new String(byteContent, 0, length));
+                }
+                System.out.println(content.toString());
+			} else {
+				 System.out.println(msg);
+			}
+			
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+}
